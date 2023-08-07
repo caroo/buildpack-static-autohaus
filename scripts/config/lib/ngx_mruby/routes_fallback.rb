29fe3e119671367c93ed49cca259706d84170478
dmaltsev <dmaltsev@brightgrove.com>
2023-08-07 19:30:49 +0300

    add autohaus fail redirects

--- scripts/config/lib/ngx_mruby/routes_fallback.rb
+++ scripts/config/lib/ngx_mruby/routes_fallback.rb
@@ -9,11 +9,20 @@ req       = Nginx::Request.new
 uri       = req.var.uri
 proxies   = config["proxies"] || {}
 redirects = config["redirects"] || {}
+brand_redirects = config["brand_redirects"] || {}
+all_cities = config['all_cities'] || []
+pagination_redirects = config['pagination_redirects'] || []
 
 if proxy = NginxConfigUtil.match_proxies(proxies.keys, uri)
   "@#{proxy}"
 elsif redirect = NginxConfigUtil.match_redirects(redirects.keys, uri)
   "@#{redirect}"
+elsif brand_redirect = NginxConfigUtil.match_brands_redirects(brand_redirects.keys, uri)
+  "@brand_#{brand_redirect}"
+elsif NginxConfigUtil.match_pagination_error(pagination_redirects, uri)
+  "@410"
+elsif NginxConfigUtil.match_dealers_error(all_cities, uri)
+  "@410"
 else
   "@404"
 end
